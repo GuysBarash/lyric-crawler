50e2b155d847cd1b2718816582830e9fe8b94847
Guy Barash <Guy.Barash@Sandisk.com>
2017-05-30 16:34:56 +0300

    One function that read all songs and the lyrics
    Signed-off-by: Guy Barash <Guy.Barash@Sandisk.com>

--- readweb.py
+++ readweb.py
@@ -55,42 +55,45 @@ def worker(url):
 # Input: an artist page with songs links
 # Output: a list of all the pages with songs (page 1, page 2 , .. )
 def get_all_urls(url):
-    urls = []
+    table_tag = r'/html/body/table[2]/tbody/tr/td/table/tbody/tr/td[2]/table/tbody/tr/td/table/tbody/tr/td[1]/table/tbody/tr/td[2]/table/tbody/tr[5]/td/table'
+    index_urls = []
+    urls = dict()
     driver = init_driver()
 
     c_url = url
     driver.get(c_url)
     html = driver.page_source
+
+    # Get 'next' button
     soup = bs4.BeautifulSoup(html, 'lxml')
     next_page = soup.find_all('a', {'class': 'artist_nav_bar'}, text=lambda (x): (x.find('>>') >= 0))
-    urls.append(c_url)
+
+    # Keep current page
+    index_urls.append(c_url)
+
+    # Get all songs from current page
+    parser = etree.HTMLParser()
+    root = etree.parse(StringIO(html), parser)
+    song_elements = root.xpath(table_tag + '//*[contains(@class, \'artist_player_songlist\')]')
+    for song in song_elements:
+        urls[song.text] = 'http://shironet.mako.co.il' + song.get('href')
+
     while len(next_page) > 0:
         c_url = r'http://shironet.mako.co.il' + next_page[0].get('href')
-        urls.append(c_url)
+        index_urls.append(c_url)
         driver.get(c_url)
         html = driver.page_source
         soup = bs4.BeautifulSoup(html, 'lxml')
         next_page = soup.find_all('a', {'class': 'artist_nav_bar'}, text=lambda (x): (x.find('>>') >= 0))
 
+        # Get all songs from current page
+        parser = etree.HTMLParser()
+        root = etree.parse(StringIO(html), parser)
+        song_elements = root.xpath(table_tag + '//*[contains(@class, \'artist_player_songlist\')]')
+        for song in song_elements:
+            urls[song.text] = 'http://shironet.mako.co.il' + song.get('href')
+
     driver.quit()
-    return urls
-
-
-# Input: an artist page with songs links
-# Output: a dict of all the songs and the links
-def get_urls_by_songs_page(url):
-    table_tag = r'/html/body/table[2]/tbody/tr/td/table/tbody/tr/td[2]/table/tbody/tr/td/table/tbody/tr/td[1]/table/tbody/tr/td[2]/table/tbody/tr[5]/td/table'
-    urls = dict()
-    driver = init_driver()
-    driver.get(url)
-    html = driver.page_source
-    driver.quit()
-    parser = etree.HTMLParser()
-    root = etree.parse(StringIO(html), parser)
-
-    song_elements = root.xpath(table_tag + '//*[contains(@class, \'artist_player_songlist\')]')
-    for song in song_elements:
-        urls[song.text] = 'http://shironet.mako.co.il' + song.get('href')
     return urls
 
 
@@ -98,22 +101,18 @@ if __name__ == "__main__":
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--mute-audio")
 
-    all_url = get_all_urls(Artist_Aviv_Geffen)
-    all_songs = dict()
-    for url in all_url:
-        all_songs.update(get_urls_by_songs_page(url))
-
+    all_url = get_all_urls(Artist_Artzi)
     counter = 0
-    for key in all_songs.keys():
-        print u"[{:^3}][{:^35}]\t{}".format(counter, key, all_songs[key])
+    for key in all_url.keys():
+        print u"[{:^3}][{:^35}]\t{}".format(counter, key, all_url[key])
         counter += 1
-    # q = Queue()
-    # p = Pool(cpu_count(), initializer=worker_init, initargs=(q, english))
-    # p.map(worker, urls)
-    # p.terminate()
-    #
-    # counter = 0
-    # while not q.empty():
-    #     item = q.get()
-    #     print u"[{}]\tTitle: {}".format(counter, item[0])
-    #     counter += 1
+        # q = Queue()
+        # p = Pool(cpu_count(), initializer=worker_init, initargs=(q, english))
+        # p.map(worker, urls)
+        # p.terminate()
+        #
+        # counter = 0
+        # while not q.empty():
+        #     item = q.get()
+        #     print u"[{}]\tTitle: {}".format(counter, item[0])
+        #     counter += 1
