dbbe91ddc407952aef953a42403db19bfe0d704e
Guy Barash <guysbarash@gmail.com>
2022-10-12 10:01:10 +0300

    -- added word normalization

--- readweb.py
+++ readweb.py
@@ -4,6 +4,8 @@ import time
 import random
 from multiprocessing import Queue, Pool, cpu_count
 import re
+
+import pandas as pd
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import bs4
@@ -15,12 +17,63 @@ import datetime
 from Artists import artist_list
 import json
 import os
+import numpy as np
+import requests
 
 global english
 global hebrew
 global logger
 english = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 hebrew = 'אבגדהוזחטיכלמנסעפצקרשת' + 'םןךףץ'
+
+from tqdm import tqdm
+
+tqdm.pandas()
+
+
+class WordNormalizer:
+    def __init__(self):
+        pass
+
+    def normalize(self, word_list, top_n=None):
+        if top_n is None:
+            word_list = word_list[:top_n]
+        request = {
+            'token': 'lptwXKtDgLEzzjW',
+            'readable': False,
+            'words': word_list,
+        }
+        print("Sending request to Hebrew NLP (Word count: {})".format(len(word_list)))
+        st = datetime.datetime.now()
+        result = requests.post('https://hebrew-nlp.co.il/service/Morphology/Analyze', json=request).json()
+        print("Got response from Hebrew NLP. Duration: {}".format(datetime.datetime.now() - st))
+
+        res = pd.DataFrame(columns=['base', 'word'], index=range(len(word_list)))
+        res['word'] = word_list
+        res['base'] = [result[i][0]['baseWord'] for i in range(len(result))]
+        return res
+
+    def _normalize_load_from_memory(self, word_list, top_n=None):
+        path = 'res.csv'
+        if os.path.exists(path):
+            res = pd.read_csv(path, index_col=0)
+        else:
+            res = self.normalize(word_list, top_n)
+            res.to_csv(path, encoding='utf-8-sig')
+
+        return res
+
+
+def clean_song_list(l):
+    lx = [lt for lt in l if bool(set(lt).intersection(hebrew))]
+
+    patterns = ['^ו']
+    patterns += ["^ת'"]
+    patterns += [r'\\']
+    for pattern in patterns:
+        lx = [re.sub(pattern, '', lt) for lt in lx]
+    lx = [lt for lt in lx if len(lt) > 1]
+    return lx
 
 
 def init_driver():
@@ -33,7 +86,7 @@ def init_driver():
 def analyze_given_song_by_link(driver, url):
     global q
     logger.log_print("Working: {}".format(url))
-    while True:
+    for i in range(6):
         try:
             driver.get(url)
             html = driver.page_source
@@ -50,8 +103,7 @@ def analyze_given_song_by_link(driver, url):
                     # line_words = [x for x in line_words if not bool(set(x).intersection(english))]
 
                     # Remove all non-hebrew words
-                    line_words = [x for x in line_words if bool(set(x).intersection(hebrew))]
-
+                    line_words = clean_song_list(line_words)
                     words += line_words
 
             # Convert song name to unicode
@@ -125,9 +177,11 @@ def read_all_songs(urls):
 
     word_cap = 300000
     for url in urls:
-        while True:
+        success = False
+        for i in range(10):
             try:
                 word_count += analyze_given_song_by_link(driver, url)
+                success = True
                 break
             except Exception as e:
                 logger.log_print("Rebooting driver")
@@ -220,25 +274,160 @@ if __name__ == "__main__":
     u_sig = datetime.datetime.now().strftime("_%H%M_%d_%m_%Y")
     logger.initThread("Report{}.txt".format(u_sig))
 
-    root_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
-    if not os.path.exists(root_path):
-        os.makedirs(root_path)
+    section_path_creation = True
+    if section_path_creation:
+        root_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+        if not os.path.exists(root_path):
+            os.makedirs(root_path)
 
-    index_path = os.path.join(root_path, 'index')
-    if not os.path.exists(index_path):
-        os.makedirs(index_path)
+        index_path = os.path.join(root_path, 'index')
+        if not os.path.exists(index_path):
+            os.makedirs(index_path)
 
-    single_thread = False
-    if single_thread:
-        print("Single Thread")
-        handle_prep(logger)
-        for unit in artist_list:
-            handle(unit)
-    else:
-        print("Starting pool")
-        p = Pool(processes=2, initializer=handle_prep, initargs=(logger,))
-        p.imap_unordered(handle, artist_list)
-        p.close()
-        p.join()
+        index_summary_path = os.path.join(root_path, 'index_summary.csv')
+        words_hist_path = os.path.join(root_path, 'words_hist.csv')
 
+    section_crawling = False
+    if section_crawling:
+        single_thread = False
+        if single_thread:
+            print("Single Thread")
+            handle_prep(logger)
+            for unit in artist_list:
+                handle(unit)
+        else:
+            print("Starting pool")
+            p = Pool(processes=2, initializer=handle_prep, initargs=(logger,))
+            p.imap_unordered(handle, artist_list)
+            p.close()
+            p.join()
+
+    section_summary = True
+    if section_summary:
+        print("Summary:")
+        root_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+        index_path = os.path.join(root_path, 'index')
+
+        index_summary = pd.DataFrame(columns=['artist', 'artist_key', 'index_page', 'songs_page', 'songs_count'])
+        for file in os.listdir(index_path):
+            if file.endswith(".json"):
+                with open(os.path.join(index_path, file), 'r', encoding='utf-8') as f:
+                    d = json.load(f)
+                    index_summary = pd.concat([index_summary, pd.DataFrame([d])], ignore_index=True)
+        if 'songs' in index_summary.columns:
+            index_summary.drop('songs', axis=1, inplace=True)
+
+        index_summary['songs_crawled'] = -1
+        index_summary['songs_failed'] = -1
+        index_summary['empty_songs'] = -1
+        index_summary['words_count'] = -1
+        index_summary['unique_words_count'] = -1
+        for file in os.listdir(root_path):
+            if file.endswith(".json"):
+                full_path = os.path.join(root_path, file)
+                with open(full_path, 'r', encoding='utf-8') as f:
+                    d = json.load(f)
+                    sr = pd.Series(d)
+                    sr.pop('songs')
+                    d_songs = d['songs'].copy()
+
+                    all_words = list()
+                    unique_words = list()
+                    for song_key in list(d_songs.keys()):
+                        l = clean_song_list(d_songs[song_key])
+                        d_songs[song_key] = l
+                        all_words.extend(l)
+                    unique_words = list(set(all_words))
+
+                    artist_key = file.replace('.json', '')
+                    songs_detected = len(d_songs)
+                    empty_songs = [k for k, v in d_songs.items() if len(v) < 1]
+                    idxs = index_summary['artist_key'] == artist_key
+                    if idxs.sum() == 0:
+                        raise Exception("Artist {} not found in index".format(artist_key))
+
+                    index_summary.loc[idxs, 'songs_page'] = full_path
+                    index_summary.loc[idxs, 'songs_crawled'] = songs_detected
+                    index_summary.loc[idxs, 'songs_failed'] = index_summary.loc[idxs, 'songs_count'] - songs_detected
+                    index_summary.loc[idxs, 'empty_songs'] = len(empty_songs)
+                    index_summary.loc[idxs, 'words_count'] = len(all_words)
+                    index_summary.loc[idxs, 'unique_words_count'] = len(unique_words)
+
+        index_summary.to_csv(index_summary_path, index=False, encoding='utf-8-sig')
+        logger.log_print("Summary file created at {}".format(index_summary_path))
+
+    section_build_kaggle = True
+    if section_build_kaggle:
+        db = pd.DataFrame(columns=['artist', 'artist_key',
+                                   'song', 'song_key',
+                                   'words', 'words count', 'unique words count',
+                                   ])
+        xdf = pd.DataFrame()
+        for artist_idx, song_file_path in tqdm(index_summary['songs_page'].items(), total=index_summary.shape[0],
+                                               desc="Building Kaggle from jsons"
+                                               ):
+            artist_key = index_summary.loc[artist_idx, 'artist_key']
+            with open(song_file_path, 'r', encoding='utf-8') as f:
+                d = json.load(f)
+                d_songs = pd.DataFrame(d)
+                d_songs['song'] = d_songs.index
+                d_songs['artist_key'] = artist_key
+                d_songs.pop('songs_count')
+                xdf = pd.concat([xdf, d_songs], ignore_index=True)
+
+        xdf_songs = xdf['songs'].progress_apply(clean_song_list)
+        j = 3
+
+    section_words_extraction = True
+    if section_words_extraction:
+        logger.log_print("Words extraction:")
+        index_summary = pd.read_csv(index_summary_path, encoding='utf-8-sig')
+
+        all_words = list()
+        unique_words = list()
+        for artist_idx, song_file_path in index_summary['songs_page'].items():
+            with open(song_file_path, 'r', encoding='utf-8') as f:
+                d = json.load(f)
+                d_songs = d['songs']
+                for song_key in list(d_songs.keys()):
+                    l = d_songs[song_key]
+                    l = clean_song_list(l)
+                    all_words.extend(l)
+
+        # Create histogram of words
+        words_hist = pd.Series(all_words).value_counts()
+        words_hist = pd.DataFrame(columns=['word', 'count'], data=words_hist.reset_index().values)
+
+        # Min words appearance = 3
+        threshold = 3
+        words_hist = words_hist[words_hist['count'] >= threshold]
+
+        # Use NLP to extract words base
+        word_normalizer = WordNormalizer()
+        normalized_words = word_normalizer.normalize(list(words_hist['word'].values))
+        bases = list(normalized_words['base'].unique())
+
+        normalized_words = pd.merge(normalized_words, words_hist, on='word')
+        normalized_words.rename(columns={'count': 'word count'}, inplace=True)
+
+        histdf = pd.DataFrame(columns=['base', 'count', 'example 1', 'example 2', 'example 3'],
+                              index=range(len(bases)))
+        for i in tqdm(range(len(bases)), desc='Words base extraction'):
+            base = bases[i]
+            subdf = normalized_words[normalized_words['base'] == base]
+            count = subdf['word count'].sum()
+            examples = list(subdf['word'].unique())
+            examples_3 = examples[:3]
+            examples_3 = np.append(examples_3, [''] * (3 - len(examples_3)))
+            histdf.loc[i, 'base'] = base
+            histdf.loc[i, 'count'] = count
+            histdf.loc[i, 'example 1'] = examples_3[0]
+            histdf.loc[i, 'example 2'] = examples_3[1]
+            histdf.loc[i, 'example 3'] = examples_3[2]
+
+        histdf.sort_values(by='count', ascending=False, inplace=True)
+        histdf.to_csv(words_hist_path, index=False, encoding='utf-8-sig')
+        logger.log_print("Words histogram file created at {}".format(words_hist_path))
+
+    logger.log_print("FINNISH")
     logger.log_close()
